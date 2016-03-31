870e764c17d3f18e8fb9dfb745f33f7b1fa74370
Iman Mohamadi <iman.mohamadi.dev@gmail.com>
2016-03-31 16:38:39 +0430

    create model method and get model working together nicely

--- src/lastdate-core.js
+++ src/lastdate-core.js
@@ -1,5 +1,19 @@
+var models = {};
+
+var makeRandomId = function makeRandomId () {
+  return Math.floor(Math.random() * 1000000);
+};
+
 module.exports = {
+  
   createModel: function () {
-    
+    var id = makeRandomId();
+    models[id] = {id: id};
+    return id;
+  },
+  
+  getModel: function (id) {
+    return models[id];
   }
+  
 };
--- test/core.test.js
+++ test/core.test.js
@@ -3,5 +3,17 @@ describe('Lastdate core', function () {
     it('any function of core exist on the page', function () {
       assert.ok(typeof lastdateCore.createModel === 'function');
     });
+    
+    it('createModel should pass the created model id', function () {
+      var id = lastdateCore.createModel();
+      assert.ok(typeof id === 'number');
+    });
+    
+    it('should be able to get specific model', function () {
+      var id = lastdateCore.createModel();
+      assert.ok(typeof id === 'number');
+      assert.ok(typeof lastdateCore.getModel(id) === 'object');
+      assert.ok(lastdateCore.getModel(id).id === id);
+    });
   });
 });
